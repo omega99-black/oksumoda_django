682eae7a283e1e0fefb513bc73b29afa036bbfa7
omega99-black <snnakesantiago@gmail.com>
2026-05-30 13:54:22 -0500

    Migracion a PostgreSQL

--- oksumoda/settings.py
+++ oksumoda/settings.py
@@ -83,11 +83,13 @@ WSGI_APPLICATION = 'oksumoda.wsgi.application'
 # ──────────────────────────────────────────────
 # BASE DE DATOS
 # ──────────────────────────────────────────────
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 # ──────────────────────────────────────────────
