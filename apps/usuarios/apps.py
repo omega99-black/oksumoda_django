7836c919e78ffc62880b5fc36a5255d35b4cb1d4
omega99-black <snnakesantiago@gmail.com>
2026-05-30 16:40:19 -0500

    Crear admin automaticamente al arrancar

--- apps/usuarios/apps.py
+++ apps/usuarios/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
 
+
 class UsuariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.usuarios'
     verbose_name = 'Usuarios'
+
+    def ready(self):
+        try:
+            from apps.usuarios.models import Usuario, Rol
+            rol_admin, _ = Rol.objects.get_or_create(nombre='ADMIN')
+            if not Usuario.objects.filter(email='admin@oksu.com').exists():
+                u = Usuario()
+                u.nombre = 'Admin'
+                u.email = 'admin@oksu.com'
+                u.set_password('admin123')
+                u.estado = 'activo'
+                u.is_staff = True
+                u.is_superuser = True
+                u.rol = rol_admin
+                u.save()
+                print("✅ Usuario admin creado automáticamente")
+        except Exception:
+            pass
