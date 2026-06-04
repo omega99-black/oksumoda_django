dc63d04b9d4bfcd27893a0793e103d8173e5aa9c
omega99-black <snnakesantiago@gmail.com>
2026-06-04 17:13:50 -0500

    Fix ALLOWED_HOSTS hardcoded

--- oksumoda/settings.py
+++ oksumoda/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ──────────────────────────────────────────────
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-cambia-esto-en-produccion')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.up.railway.app',
